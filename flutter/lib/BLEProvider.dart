34367e16f45c095d6ab8c8fd004ffe89082b25e2
Antigones <>
2021-01-25 22:22:34 +0100

    first commit

---
+++ flutter/lib/BLEProvider.dart
@@ -0,0 +1,94 @@
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:provider/provider.dart';
+class BLEProvider with ChangeNotifier  {
+  final FlutterReactiveBle _ble = FlutterReactiveBle();
+  StreamSubscription _subscription;
+  StreamSubscription<ConnectionStateUpdate> _connection;
+  String _message;
+
+  BLEProvider() {
+    _connectBLE();
+  }
+
+  void _connectBLE()  {
+    /*setState(() {
+      temperatureStr = 'Loading';
+    });*/
+    _message = "Loading";
+    notifyListeners();
+    _subscription?.cancel();
+    _subscription = _ble.scanForDevices(
+        withServices: [],
+        scanMode: ScanMode.lowLatency,
+        requireLocationServicesEnabled: true).listen((device) async {
+      if (device.name == 'Nano33BLESENSE') {
+        print('Nano33BLESENSE found!');
+        if (_connection != null) {
+          await _connection.cancel();
+        }
+
+        _connection = _ble
+            .connectToDevice(
+          id: device.id,
+        )
+            .listen((connectionState) async {
+          // Handle connection state updates
+          print('connection state:');
+          print(connectionState.connectionState);
+          if (connectionState.connectionState ==
+              DeviceConnectionState.connected) {
+            final characteristic = QualifiedCharacteristic(
+                serviceId: Uuid.parse("181A"),
+                characteristicId: Uuid.parse("2A6E"),
+                deviceId: device.id);
+            _ble.subscribeToCharacteristic(characteristic).listen((data) {
+              // code to handle incoming data
+              print(data);
+              _message = data[0].toString()+ '°';
+              notifyListeners();
+              /*setState(() {
+                temperature = data[0];
+                temperatureStr = temperature.toString() + '°';
+              });*/
+            }, onError: (dynamic error) {
+              // code to handle errors
+              print('error subscribing characteristic!');
+              print(error.toString());
+             /* setState(() {
+                temperatureStr = error.toString();
+              });*/
+            });
+           // print('disconnected');
+          }
+        }, onError: (dynamic error) {
+          // Handle a possible error
+          print('error connecting!');
+          print(error.toString());
+         /* setState(() {
+            temperatureStr = error.toString();
+          });*/
+        });
+      }
+    }, onError: (error) {
+      print('error scanning!');
+      print(error.toString());
+      /*setState(() {
+        temperatureStr = error.toString();
+      });*/
+    });
+
+  }
+
+  void _disconnect() async {
+    _subscription?.cancel();
+    if (_connection != null) {
+      await _connection.cancel();
+    }
+  }
+
+  String get message => _message;
+  StreamSubscription get bleSubscription => _subscription;
+}
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,11 +1,21 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_arduino_nano_33_ble/BLEProvider.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => BLEProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,105 +28,45 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Arduino Temperature'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final FlutterReactiveBle _ble = FlutterReactiveBle();
-  StreamSubscription _subscription;
-  StreamSubscription<ConnectionStateUpdate> _connection;
+  final BLEProvider _bleProvider = BLEProvider();
   int temperature;
   String temperatureStr = "Hello";
 
-  void _disconnect() async {
-    _subscription?.cancel();
-    if (_connection != null) {
-      await _connection.cancel();
-    }
-  }
-
-  void _connectBLE() {
-    setState(() {
-      temperatureStr = 'Loading';
-    });
-    _disconnect();
-    _subscription = _ble.scanForDevices(
-        withServices: [],
-        scanMode: ScanMode.lowLatency,
-        requireLocationServicesEnabled: true).listen((device) {
-      if (device.name == 'Nano33BLESENSE') {
-        print('Nano33BLESENSE found!');
-        _connection = _ble
-            .connectToDevice(
-          id: device.id,
-        )
-            .listen((connectionState) async {
-          // Handle connection state updates
-          print('connection state:');
-          print(connectionState.connectionState);
-          if (connectionState.connectionState ==
-              DeviceConnectionState.connected) {
-            final characteristic = QualifiedCharacteristic(
-                serviceId: Uuid.parse("181A"),
-                characteristicId: Uuid.parse("2A6E"),
-                deviceId: device.id);
-            final response = await _ble.readCharacteristic(characteristic);
-            print(response);
-            setState(() {
-              temperature = response[0];
-              temperatureStr = temperature.toString() + '°';
-            });
-            _disconnect();
-            print('disconnected');
-          }
-        }, onError: (dynamic error) {
-          // Handle a possible error
-          print(error.toString());
-        });
-      }
-    }, onError: (error) {
-      print('error!');
-      print(error.toString());
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [Color(0xffffdf6f), Color(0xffeb2d95)])),
-        child: Center(
-          child: Text(
-            temperatureStr,
-            style: GoogleFonts.anton(
-                textStyle: Theme.of(context)
-                    .textTheme
-                    .headline1
-                    .copyWith(color: Colors.white)),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
+        body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [Color(0xffffdf6f), Color(0xffeb2d95)])),
+            child: Center(child: Consumer<BLEProvider>(
+              builder: (context, ble, child) {
+                return Text(
+                  ble.message,
+                  style: GoogleFonts.anton(
+                      textStyle: Theme.of(context)
+                          .textTheme
+                          .headline1
+                          .copyWith(color: Colors.white)),
+                );
+              },
+            )))
+        /*floatingActionButton: FloatingActionButton(
         onPressed: _connectBLE,
         tooltip: 'Increment',
         backgroundColor: Color(0xFF74A4BC),
         child: Icon(Icons.loop),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ), */ // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
